a62e48867575e85e7ca151fb1054dc5a58563467
Max-F <max.fullmer@outlook.com>
2019-07-15 16:23:17 -0700

    July 15th, 2019

--- src/Counter.js
+++ src/Counter.js
@@ -1,51 +1,78 @@
 import React, { Component } from 'react';
+import store, { INCREMENT, DECREMENT, UNDO } from './store';
 
 class Counter extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      store: store.getState()
+    };
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
+    this.undo = this.undo.bind(this)
+  }
+
+  componentDidMount() {
+    store.subscribe(() => {
+      this.setState({
+        store: store.getState()
+      })
+    })
+  }
+
+  increment(amount) {
+    store.dispatch({type: INCREMENT, payload: amount})
+  }
+
+  decrement(amount) {
+    store.dispatch({type: DECREMENT, payload: amount})
+  }
+
+  undo() {
+    store.dispatch({type: UNDO})
   }
   render() {
+    const {currentValue, previousValues, futureValues} = this.state.store;
     return (
       <div className="app">
         <section className="counter">
-          <h1 className="counter__current-value">{0}</h1>
+          <h1 className="counter__current-value">{currentValue}</h1>
           <div className="counter__button-wrapper">
             <button
               className="counter__button increment-one"
-              onClick={() => null}
+              onClick={() => this.increment(1)}
             >
               +1
             </button>
             <button
               className="counter__button increment-five"
-              onClick={() => null}
+              onClick={() => this.increment(5)}
             >
               +5
             </button>
             <button
               className="counter__button decrement-one"
-              onClick={() => null}
+              onClick={() => this.decrement(1)}
             >
               -1
             </button>
             <button
               className="counter__button decrement-five"
-              onClick={() => null}
+              onClick={() => this.decrement(5)}
             >
               -5
             </button>
             <br />
             <button
               className="counter__button undo"
-              disabled={true}
-              onClick={() => null}
+              disabled={previousValues.length === 0}
+              onClick={this.undo}
             >
               Undo
             </button>
             <button
               className="counter__button redo"
-              disabled={true}
+              disabled={futureValues.length === 0}
               onClick={() => null}
             >
               Redo
@@ -53,7 +80,7 @@ class Counter extends Component {
           </div>
         </section>
         <section className="state">
-          <pre>{JSON.stringify(this.state, null, 2)}</pre>
+          <pre>{JSON.stringify(this.state.store, null, 2)}</pre>
         </section>
       </div>
     );
---
+++ src/store.js
@@ -0,0 +1,41 @@
+import {createStore} from 'redux';
+
+const initialState = {
+    currentValue: 0,
+    previousValues: [],
+    futureValues: [],
+
+}
+
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
+export const UNDO = "UNDO";
+export const REDO = "REDO";
+
+function counter(state = initialState, action){
+    switch(action.type) {
+        case INCREMENT:
+            return {
+                futureValues: [],
+                currentValue: state.currentValue + action.payload,
+                previousValues: [state.currentValue, ...state.previousValues]
+            }
+        case DECREMENT:
+            return {
+                futureValues: [],
+                currentValue: state.currentValue - action.payload,
+                previousValues: [state.currentValue, ...state.previousValues]
+            }
+        case UNDO:
+            return {
+                currentValue: state.previousValues[0],
+                futureValues: [state.currentValue, ...state.futureValues],
+                previousValues: state.previousValues.slice(1)
+            }
+        default:
+            return state;
+    }
+}
+
+
+export default createStore(counter);
